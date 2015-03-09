e1d809d8db77aa9534fcbce9be8f4bfe695fadc8
Hoàng Minh Thắng <p@banphim.net>
2015-03-09 22:14:11 +0700

    new data structure for automata

--- src/bargaining_population/automaton.cljs
+++ src/bargaining_population/automaton.cljs
@@ -1,40 +1,20 @@
 (ns bargaining-population.automaton)
 
-(defn pure-strategy [strategy]
-  {:name (keyword (str "all-" (name strategy)))
-   :state-id 1
-   :state-ids #{1}
-   :state-strategies {1 strategy}
-   :state-transitions [[1 :high 1]
-                       [1 :medium 1]
-                       [1 :low 1]]})
-
-(def accommodator
-  {:name :accommodator
-   :state-id 2
-   :state-ids #{1 2 3}
-   :state-strategies {1 :high
-                      2 :medium
-                      3 :low}
-   :state-transitions [[1 :high 3]
-                       [1 :medium 2]
-                       [1 :low 1]
-                       [2 :high 3]
-                       [2 :medium 2]
-                       [2 :low 1]
-                       [3 :high 3]
-                       [3 :medium 2]
-                       [3 :low 1]]})
-
 (def initial-automaton
-  {:all-high (pure-strategy :high)
-   :all-medium (pure-strategy :medium)
-   :all-low (pure-strategy :low)
-   :accommodator accommodator})
+  {:high         {:strategy :high
+                  :accommodator? false}
+   :medium       {:strategy :medium
+                  :accommodator? false}
+   :low          {:strategy :low
+                  :accommodator? false}
+   :accommodator {:strategy :medium
+                  :accommodator? true}})
 
-(defn fsm->strategy [fsm]
-  (get (-> fsm :state-strategies)
-       (-> fsm :state-id)))
+(defn automaton-name
+  [{:keys [strategy accommodator?]}]
+  (if accommodator?
+    :accommodator
+    strategy))
 
 (defn transit-state [{:keys [state-id state-transitions] :as fsm} opponent-state]
   (first
