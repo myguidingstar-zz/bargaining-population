88154c802286c389d5f8465c180d22d0f0a2a07f
Hoàng Minh Thắng <p@banphim.net>
2015-03-11 08:50:56 +0700

    add list->points util function

--- src/bargaining_population/app.cljs
+++ src/bargaining_population/app.cljs
@@ -214,6 +214,17 @@ cycles'. The last cycle is the one that will be fed to the next
           :x2 x2
           :y2 y2}])
 
+(defn list->points
+  "Generates a list of points that can be fed to chart.
+  Arguments:
+
+    - x-step: constant distance between two adjacent points' x values.
+    - height: the height of the chart.
+    - ys: list of main values, each of which is between 0 and 1."
+  [x-step height ys]
+  (-> #(vector (* x-step (inc %1)) (- height (* height %2)))
+      (map-indexed ys)))
+
 (rum/defc column < rum/static
   [i payoff-mean selected]
   [:div.column {:key i
