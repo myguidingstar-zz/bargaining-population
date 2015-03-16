5436fb5a3e7e9eb3da8c8eaf54146c2aaa37c26e
Hoàng Minh Thắng <p@banphim.net>
2015-03-16 23:11:45 +0700

    use computation-input-channel in stop

--- src/bargaining_population/app.cljs
+++ src/bargaining_population/app.cljs
@@ -120,8 +120,8 @@ cycles'. The last cycle is the one that will be fed to the next
 
 (defn stop []
   (reset! status :stopped)
-  (toggle mixer {computation-output-channel {:mute true
-                                             :pause false}})
+  (go (<! computation-input-channel)
+      (>! computation-input-channel false))
   nil)
 
 (defn start-worker []
